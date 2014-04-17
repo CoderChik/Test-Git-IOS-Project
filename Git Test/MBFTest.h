f47120128c26551f6749510f0a6018fe32eb0868
Jamie Berry <jamieberry@Jamies-MacBook-Air.local>
2014-04-17 18:21:23 -0500

    added a test class

---
+++ Git Test/MBFTest.h
@@ -0,0 +1,13 @@
+//
+//  MBFTest.h
+//  Git Test
+//
+//  Created by Jamie Berry on 4/17/14.
+//  Copyright (c) 2014 Jamie Berry. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MBFTest : NSObject
+
+@end
